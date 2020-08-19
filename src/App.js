81082ac5e1f8f546adf4afc00f7173ec9e54b449
Kibotot <KibototV2@gmail.com>
2020-08-19 20:55:32 +0200

    startign files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import arrow from "./images/arrow-right.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      <div className="hero-inner">
+        <div className="hero-content">
+          <div className="hero-content-inner">
+            <h1>
+              <div className="hero-content-line">
+                <div className="hero-content-line-inner">
+                  Relieving the burden
+                </div>
+              </div>
+              <div className="hero-content-line">
+                <div className="hero-content-line-inner">of disease caused</div>
+              </div>
+              <div className="hero-content-line">
+                <div className="hero-content-line-inner">by behavior.</div>
+              </div>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              voluptatibus natus veniam dolor commodi laboriosam sint minus
+              consequuntur optio, voluptatem nobis voluptatum, quibusdam, unde
+              praesentium harum corrupti fugit ut ipsa.
+            </p>
+            <div className="button-row">
+              <button className="explore-button">
+                Explore{" "}
+                <div className="arrow-icon">
+                  <img src={arrow} alt="arrow" />
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="hero-images"></div>
+      </div>
     </div>
   );
 }
