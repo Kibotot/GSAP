ac1f64307b8f36e43ff4b8ffde3c3b5b98eae69d
Kibotot <KibototV2@gmail.com>
2020-08-20 09:56:29 +0200

    content added for animations

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
 import React from "react";
 import "./App.scss";
 import arrow from "./images/arrow-right.svg";
+import imgGirl from "./images/girl.webp";
+import imgBoy from "./images/boy.webp";
 
 function App() {
   return (
     <div className="hero">
-      <div className="hero-inner">
-        <div className="hero-content">
-          <div className="hero-content-inner">
-            <h1>
-              <div className="hero-content-line">
-                <div className="hero-content-line-inner">
-                  Relieving the burden
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div className="hero-content-inner">
+              <h1>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    Relieving the burden
+                  </div>
                 </div>
-              </div>
-              <div className="hero-content-line">
-                <div className="hero-content-line-inner">of disease caused</div>
-              </div>
-              <div className="hero-content-line">
-                <div className="hero-content-line-inner">by behavior.</div>
-              </div>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-              voluptatibus natus veniam dolor commodi laboriosam sint minus
-              consequuntur optio, voluptatem nobis voluptatum, quibusdam, unde
-              praesentium harum corrupti fugit ut ipsa.
-            </p>
-            <div className="button-row">
-              <button className="explore-button">
-                Explore{" "}
-                <div className="arrow-icon">
-                  <img src={arrow} alt="arrow" />
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    of disease caused
+                  </div>
                 </div>
-              </button>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">by behaviors.</div>
+                </div>
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                voluptatibus natus veniam dolor commodi laboriosam sint minus
+                consequuntur optio, voluptatem nobis voluptatum, quibusdam, unde
+                praesentium harum corrupti fugit ut ipsa.
+              </p>
+              <div className="button-row">
+                <button className="explore-button">
+                  Explore{" "}
+                  <div className="arrow-icon">
+                    <img src={arrow} alt="arrow" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="hero-images">
+            <div className="hero-images-inner">
+              <div className="hero-image girl">
+                <img src={imgGirl} alt="girl" />
+              </div>
+              <div className="hero-image boy">
+                <img src={imgBoy} alt="boy" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="hero-images"></div>
       </div>
     </div>
   );
